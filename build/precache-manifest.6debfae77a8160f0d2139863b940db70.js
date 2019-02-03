c8109b4475beb57b5f07fc3c21b25a7524d9ce4d
saiqiong <saiqiong@saiqiongs-MacBook-Pro.local>
2019-02-03 21:35:32 +0100

    well well a whole wkd work

---
+++ build/precache-manifest.6debfae77a8160f0d2139863b940db70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "081a46b614f0721fbcbe",
+    "url": "/static/js/main.081a46b6.chunk.js"
+  },
+  {
+    "revision": "2a1eea3d3b33641ab1a9",
+    "url": "/static/js/1.2a1eea3d.chunk.js"
+  },
+  {
+    "revision": "081a46b614f0721fbcbe",
+    "url": "/static/css/main.01bc3fcb.chunk.css"
+  },
+  {
+    "revision": "9e3a7904415e18e7e5287cc78b3cf43b",
+    "url": "/index.html"
+  }
+];
